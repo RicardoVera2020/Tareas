bf862a17ab7c0a0c6dd9acb97e95bcf672599bad
ricardo <ricardo@HP360.mshome.net>
2023-04-17 12:48:53 -0400

    tarea 1 dia 6 modulo 2

---
+++ tareas/fechas/src/cl/ricardovera/Fechas.java
@@ -0,0 +1,45 @@
+package cl.ricardovera;
+
+	import java.util.Scanner;
+
+	public class Fechas {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Ingrese la primera fecha como DD/MM/AAAA: ");
+	        String fecha1 = scanner.nextLine();
+	        System.out.println("Ingrese la segunda fecha como DD/MM/AAAA: ");
+	        String fecha2 = scanner.nextLine();
+
+	        int dia1 = Integer.parseInt(fecha1.substring(0, 2));
+	        int mes1 = Integer.parseInt(fecha1.substring(3, 5));
+	        int anio1 = Integer.parseInt(fecha1.substring(6));
+
+	        int dia2 = Integer.parseInt(fecha2.substring(0, 2));
+	        int mes2 = Integer.parseInt(fecha2.substring(3, 5));
+	        int anio2 = Integer.parseInt(fecha2.substring(6));
+
+	        if (anio1 > anio2) {
+	            System.out.println("La persona 1 es mayor.");
+	        } else if (anio2 > anio1) {
+	            System.out.println("La persona 2 es mayor.");
+	        } else {
+	            if (mes1 > mes2) {
+	                System.out.println("La persona 1 es mayor.");
+	            } else if (mes2 > mes1) {
+	                System.out.println("La persona 2 es mayor.");
+	            } else {
+	                if (dia1 > dia2) {
+	                    System.out.println("La persona 1 es mayor.");
+	                } else if (dia2 > dia1) {
+	                    System.out.println("La persona 2 es mayor.");
+	                } else {
+	                    System.out.println("Tienen la misma edad.");
+	                }
+	            }
+	        }
+	        scanner.close();
+	    }
+	}
+
+
